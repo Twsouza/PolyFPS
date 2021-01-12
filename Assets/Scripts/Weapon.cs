ea7ce40090be7dac2687dff31cfabc8962770f4c
Taynan Souza <taynan.tws@gmail.com>
2021-01-12 18:18:23 -0300

    179. Creating Shooting Hit Effect

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,6 +9,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 10f;
     [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitEffect;
 
     void Update()
     {
@@ -34,7 +35,7 @@ public class Weapon : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
-            Debug.Log("I hit " + hit.transform.name);
+            CreateHitImpact(hit);
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
 
             if (target == null)
@@ -44,5 +45,11 @@ public class Weapon : MonoBehaviour
 
             target.TakeDamage(damage);
         }
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, 1f);
     }
 }
