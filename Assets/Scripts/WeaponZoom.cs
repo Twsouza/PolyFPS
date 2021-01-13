f55febdbb0230064cb2fde2b08607ae312fb9c8f
Taynan Souza <taynan.tws@gmail.com>
2021-01-13 13:55:22 -0300

    193. Weapon Zoom - Mouse Sensitivity

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -1,21 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
-    [SerializeField] Camera fpsCamera;
     [SerializeField] float zoomedOutFOV = 60f;
     [SerializeField] float zoomedInFOV = 20f;
+    [SerializeField] float zoomedOutSensitivity = 2f;
+    [SerializeField] float zoomedInSensitivity = .5f;
+
+    RigidbodyFirstPersonController fpsController;
 
     bool zoomedInToogle = false;
+
+    private void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(1))
         {
             zoomedInToogle = !zoomedInToogle;
-            fpsCamera.fieldOfView = zoomedInToogle ? zoomedInFOV : zoomedOutFOV;
+            if (zoomedInToogle)
+            {
+                Camera.main.fieldOfView = zoomedInFOV;
+                fpsController.mouseLook.XSensitivity = zoomedInSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomedInSensitivity;
+            }
+            else
+            {
+                Camera.main.fieldOfView = zoomedOutFOV;
+                fpsController.mouseLook.XSensitivity = zoomedOutSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomedOutSensitivity;
+            }
         }
     }
 }
