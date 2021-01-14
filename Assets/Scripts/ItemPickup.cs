e8b8cfc82e530dde79f664cb898bb608064f8869
Taynan Souza <taynan.tws@gmail.com>
2021-01-14 15:01:47 -0300

    Game should have an Objective and an End.
    The Player has to pickup the objectives:
    Screwdriver, Wirecutter and Wrench.
    After get all items, he can scape with the Helicopter.

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    [SerializeField] ObjectiveType objective;
+    [SerializeField] TextMeshProUGUI objectiveText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            objectiveText.color = Color.green;
+            FindObjectOfType<Objective>().SetObjectiveCompleted(objective);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Objective.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ObjectiveType
+{
+    ScrewDriver,
+    Wirecutter,
+    Wrench
+}
+
+public class Objective : MonoBehaviour
+{
+    [SerializeField] CompletedObjectives[] objectives;
+    [SerializeField] Canvas gameFinished;
+
+    [System.Serializable]
+    private class CompletedObjectives
+    {
+        public ObjectiveType objective;
+        public bool completed;
+    }
+
+    private void Start()
+    {
+        gameFinished.enabled = false;
+    }
+
+    public void SetObjectiveCompleted(ObjectiveType newObjective)
+    {
+        foreach (CompletedObjectives objective in objectives)
+        {
+            if (objective.objective == newObjective)
+            {
+                objective.completed = true;
+                return;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && IsObjectivesCompleted())
+        {
+            GameFinished();
+        }
+    }
+
+    private bool IsObjectivesCompleted()
+    {
+        foreach (CompletedObjectives objective in objectives)
+        {
+            if (!objective.completed)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void GameFinished()
+    {
+        gameFinished.enabled = true;
+        Time.timeScale = 0;
+        FindObjectOfType<WeaponSwitcher>().enabled = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
